8ada9fb42756536452a1132e14a09cf7c760e0a4
Tomtom <tom8u4286@gmail.com>
2016-11-05 18:37:41 +0800

    Change the directory of unique_words.txt into data/

--- plot/yelp_minor/GooglePretrainedTest.py
+++ plot/yelp_minor/GooglePretrainedTest.py
@@ -4,7 +4,7 @@ model = gensim.models.Word2Vec.load_word2vec_format('../yelp/GoogleNews-vectors-
 unique_words = list(model.vocab)
 print unique_words
 
-with open("unique_words.txt", "w+") as f:
+with open("../../data/unique_words.txt", "w+") as f:
     for word in unique_words:
         f.write( word.encode('utf8') + "\n")
 
