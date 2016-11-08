43a9ffe2a51afeabc370a5ad6b1159a87961fe0e
Tomtom <tom8u4286@gmail.com>
2016-11-09 07:40:27 +0800

    FindNearestSentiment.py

--- plot/starRepresentation/FindNearestSentiment.py
+++ plot/starRepresentation/FindNearestSentiment.py
@@ -1,13 +1,63 @@
 import json
-
+import numpy as np
+from sklearn.metrics import pairwise_distances
+from scipy.spatial.distance import cosine
 
 class FindNearestSentiment:
 
     def __init__(self):
         self.w = "../../data/coreProcess_word2vec_stars/unique_words_word2vec.txt"
-        self.vec2 = "../../data/coreProcess_word2vec_stars/"
+        self.vec = "../../data/coreProcess_word2vec_stars/vectors300_word2vec.txt"
         self.lexicon = "../../data/sentiment_statistics/total.json"
 
+        self.sentiment_list = []
 
-    def Caculate(self):
+        self.testing = 1
 
+    def get_sentiment_list(self):
+        "This function aims to find the sentiment words which appears more than 100 times."
+
+        f = open(self.lexicon)
+        lexicon = json.load(f)
+
+        for word in lexicon:
+            if word["count"] >= 100:
+                self.sentiment_list.append(word["word"])
+
+        if self.testing == 1:
+            print "Type of lexicon: "+ type(lexicon)
+            print "Length of lexicon list:"+ len(self.sentiment_list)
+
+    def caculate(self):
+        "This function aims to caculate the cosine similarity of stars and sentiment words."
+
+        star_dic = {}
+        unique_words = [line.rstrip('\n') for line in open(self.w)]
+        print "start putting vecs"
+        vec = [json.loads(line) for line in open(self.vec)]
+
+        one = unique_words.index("1star")
+        two = unique_words.index("2star")
+        three = unique_words.index("3star")
+        four = unique_words.index("4star")
+        five = unique_words.index("5star")
+
+        print "start putting np-array"
+        A = np.array(vec)
+        cos_array = 1-pairwise_distances(A, metric="cosine")
+
+        one_array = cos_array[one]
+        two_array = cos_array[two]
+        three_array = cos_array[three]
+        four_array = cos_array[four]
+        five_array = cos_array[five]
+
+        np.argsort(one_array)[-10:]
+        np.argsort(two_array)[-10:]
+        np.argsort(three_array)[-10:]
+        np.argsort(four_array)[-10:]
+        np.argsort(five_array)[-10:]
+
+if __name__ == '__main__':
+    findNearest = FindNearestSentiment()
+    findNearest.caculate()
