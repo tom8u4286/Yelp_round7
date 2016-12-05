e8958df1e861c326f55c3d7f265caadd40a18310
Yi-Fan Chu <yfchu@clip4.cs.nccu.edu.tw>
2016-12-06 00:16:08 +0800

    correlation2.py

--- Line/runGlove.sh
+++ Line/runGlove.sh
@@ -1,6 +1,6 @@
 
 #for i in ../data/backend_reviews/restaurant_1_first100.txt
-for i in ../data/backend_reviews/Bangkok_1.txt
+for i in ../data/backend_reviews/restaurant_1.txt
 do
     ts -n -f sh -c "python glove.py $i" &
 done
